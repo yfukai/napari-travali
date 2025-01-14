58e09578cc002b02cb64bc98961b10dc72d53518
Yohsuke Fukai <ysk@yfukai.net>
2025-01-14 14:25:05 +0900

    working

--- src/napari_travali2/_stateful_widget.py
+++ src/napari_travali2/_stateful_widget.py
@@ -61,7 +61,8 @@ class StateMachineWidget(Container):
                 logger.info(f"clicked at {cords} at frame {frame} and label value {val}")
                 self.track_clicked(frame, val)
         self._label_layer.mouse_drag_callbacks.append(track_clicked)
-        
+    
+    @log_error    
     def update_viewer_status(self,*_args):
         self._state_label.value = ("========================\n"
                                   f"Current State: {self.state.name}\n"
@@ -73,21 +74,68 @@ class StateMachineWidget(Container):
         else:
             self._label_layer.show_selected_label = False
         
+    @log_error    
     def select_track(self,frame,val):
         logger.info(f"Track selected: frame {frame} value {val}")
         assert self.txn is None
         self.txn = ts.Transaction()
         self._label_layer.selected_label = val
         
+    @log_error    
     def finalize_track(self):
         logger.info("Track finalized")
         self.txn.commit_sync()
         self.txn = None
         self._label_layer.selected_label = 0
     
+    @log_error    
     def abort_transaction(self):
         logger.info("Transaction aborted")
         self.txn.abort()
         self.txn = None
         self._label_layer.selected_label = 0
         
+    @log_error
+    def label_redraw_enter_valid(self):
+        iT = self.viewer.dims.current_step[0]
+        # return True if:
+        # - this timeframe is in target_T
+        # - segment_labels is not NOSEL_VALUE in either of this, previous, next target_T
+        if not iT in self.target_Ts:
+            logger.info("this frame is not in target_Ts")
+            return False
+        previous_iT = self.target_Ts[max(0, self.target_Ts.index(iT) - 1)]
+        next_iT = self.target_Ts[
+            min(len(self.target_Ts) - 1, self.target_Ts.index(iT) + 1)
+        ]
+        if (
+            self.segment_labels[iT] == NOSEL_VALUE
+            and self.segment_labels[previous_iT] == NOSEL_VALUE
+            and self.segment_labels[next_iT] == NOSEL_VALUE
+            #            not np.any(self.sel_label_layer.data[iT] == 1)
+            #            and not np.any(self.sel_label_layer.data[min(iT + 1, self.sizeT)] == 1)
+            #            and not np.any(self.sel_label_layer.data[max(iT - 1, 0)] == 1)
+        ):
+            logger.info("track does not exist in connected timeframe")
+            return False
+        else:
+            logger.info("redraw valid")
+            return True
+
+    @log_error
+    def check_drawn_label(self):
+        return np.any(self.redraw_label_layer.data == 1)
+
+    @log_error
+    def label_redraw_finish(self):
+        logger.info("label redraw finish")
+        iT = self.viewer.dims.current_step[0]
+        logger.info("label redraw finish")
+        self.sel_label_layer.data[iT] = 0
+        self.sel_label_layer.data[iT] = self.redraw_label_layer.data == 1
+        self.label_edited[iT] = True
+        if self.segment_labels[iT] == NOSEL_VALUE:
+            self.segment_labels[iT] = NEW_LABEL_VALUE
+        else:
+            if ask_draw_label(self.viewer) == "new":
+                self.segment_labels[iT] = NEW_LABEL_VALUE
