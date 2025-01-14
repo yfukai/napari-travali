7ed4ec621757f81c314e36883cce88dd04b9f47d
Yohsuke Fukai <ysk@yfukai.net>
2025-01-14 10:08:12 +0900

    keytype is not working

--- src/napari_travali2/_stateful_widget.py
+++ src/napari_travali2/_stateful_widget.py
@@ -1,29 +1,28 @@
 from transitions import Machine
 from qtpy.QtWidgets import QVBoxLayout, QWidget, QLabel
-from magicgui.widgets import Container, create_widget
+from magicgui.widgets import Container, create_widget, Label
 from napari import Viewer
-from napari.types import LabelsData
+from napari.layers import Labels as LabelsLayer
 from ._transitions import ViewerState, TRANSITIONS
 from ._logging import logger, log_error
 import numpy as np
 
-class StateMachineWidget(QWidget):
-    def __init__(self, viewer: Viewer):
+class StateMachineWidget(Container):
+    def __init__(self, viewer: "napari.viewer.Viewer", ):
         super().__init__()
 
-        self.viewer = viewer
-        self.layer_select = create_widget(annotation=LabelsData, label="Select a label layer")
+        self._viewer = viewer
+        self._layer_select = create_widget(annotation=LabelsLayer, label="Select a label layer")
         # Set up the layout
-        self.layout = QVBoxLayout()
 
         # Add a label to show the current state
-        self.state_label = QLabel("Current State: before initialization.")
+        self._state_label = Label(value=f"Current State: {ViewerState.ALL_LABEL}")
+        #self._state_label.setText(f"Current State: {ViewerState.ALL_LABEL}")
+        self.extend([
+            self._layer_select,
+            self._state_label
+        ])
         
-        self.layout.addWidget(self.layer_select.native)
-        self.layout.addWidget(self.state_label)
-        self.setLayout(self.layout)
-
-        # Define the state machine
         self.states = ["idle", "running", "paused"]
         self.machine = Machine(model=self, 
                                states=ViewerState, 
@@ -33,15 +32,16 @@ class StateMachineWidget(QWidget):
                                ignore_invalid_triggers=True)
         
 
-        self.viewer.bind_key("Enter", lambda _viewer: self.Enter_typed(), overwrite=True)
-        self.viewer.bind_key("r", lambda _viewer: self.r_typed(), overwrite=True)
-        self.viewer.bind_key("s", lambda _viewer: self.s_typed(), overwrite=True)
-        self.viewer.bind_key("d", lambda _viewer: self.d_typed(), overwrite=True)
-        self.viewer.bind_key("t", lambda _viewer: self.t_typed(), overwrite=True)
-        self.viewer.bind_key("Escape", lambda _viewer: self.Escape_typed(), overwrite=True)
+        self._viewer.bind_key("Enter", lambda _viewer: self.Enter_typed(), overwrite=True)
+        self._viewer.bind_key("r", lambda _viewer: self.r_typed(), overwrite=True)
+        self._viewer.bind_key("s", lambda _viewer: self.s_typed(), overwrite=True)
+        self._viewer.bind_key("d", lambda _viewer: self.d_typed(), overwrite=True)
+        self._viewer.bind_key("t", lambda _viewer: self.t_typed(), overwrite=True)
+        self._viewer.bind_key("Escape", lambda _viewer: self.Escape_typed(), overwrite=True)
         # getting the current layer from the layer_select
-        self.layer_select.changed.connect(self.layer_changed)
-        print(self.layer_select)
+        self._layer_select.changed.connect(self.layer_changed)
+        self.layer_changed(self._layer_select.value)
+        
         #layer = 
         
     def layer_changed(self, layer):
@@ -58,12 +58,12 @@ class StateMachineWidget(QWidget):
             if val is None:
                 return
             if val != 0:
-                msg = f"clicked at {cords}"
                 frame = cords[0]
-                logger.info("%s %i %s", msg, frame, val)
+                logger.info(f"clicked at {cords} at frame {frame} and label value {val}")
                 self.track_clicked(frame, val)
         self.label_layer.mouse_drag_callbacks.append(track_clicked)
         
     def update_viewer_status(self):
-        self.state_label.setText(f"Current State: {self.state}")
+        print(f"Current State: {self.state}")
+        #self._state_label.value = f"Current State: {self.state}"
 
