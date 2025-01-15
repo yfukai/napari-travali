992ca5eda52e0d58d712222dbe46a8c768430f44
Yohsuke Fukai <ysk@yfukai.net>
2025-01-15 14:40:17 +0900

    add daughter edit steps

--- src/napari_travali2/_stateful_widget.py
+++ src/napari_travali2/_stateful_widget.py
@@ -5,7 +5,7 @@ from napari import Viewer
 from napari.layers import Labels as LabelsLayer
 from ._transitions import ViewerState, TRANSITIONS, STATE_EXPLANATION
 from ._logging import logger, log_error
-from ._gui_utils import choose_direction_by_mbox, get_annotation_of_track_end
+from ._gui_utils import choose_direction_by_mbox, get_annotation_of_track_end,choose_division_by_mbox
 import numpy as np
 import tensorstore as ts
 from copy import deepcopy
@@ -101,7 +101,6 @@ class StateMachineWidget(Container):
             layer.visible = visible
         active_layer = self._label_layer if visibility[0] else self._redraw_layer
         self._viewer.layers.selection.active = active_layer
-        
             
         if self.state == ViewerState.LABEL_REDRAW:
             self._redraw_layer.data = np.zeros_like(self._redraw_layer.data)
@@ -179,15 +178,31 @@ class StateMachineWidget(Container):
         return is_valid
     
     @log_error
-    def switch_track(self, frame, val):
+    def switch_track(self, frame, switch_target_label):
+        track_bboxes_df = self.ta._get_track_bboxes(self._selected_label).reset_index()
+        frames = track_bboxes_df["frame"].values
         direction = choose_direction_by_mbox(self._viewer)
         if not direction:
             logger.info("Switch cancelled")
             return
         elif direction == "forward":
+            if frame <= frames.min():
+                logger.info("Cannot switch forward, since the track does not exist in the previous frame")
+                return
             logger.info("Switching forward")
+            self.ta.break_track(frame, self._selected_label, True, self.txn)
+            self.ta.break_track(frame, switch_target_label, True, self.txn, 
+                                new_trackid=self._selected_label)
         elif direction == "backward":
+            if frame >= frames.max():
+                logger.info("Cannot switch backward, since the track does not exist in the next frame")
+                return
             logger.info("Switching backward")
+            self.ta.break_track(frame+1, self._selected_label, False, self.txn)
+            self.ta.break_track(frame+1, switch_target_label, False, self.txn, 
+                                new_trackid=self._selected_label)
+        self.ta.cleanup_single_daughter_splits()
+        
 
     ################ Mark termination ################
     @log_error
@@ -210,3 +225,63 @@ class StateMachineWidget(Container):
             self.ta.terminate_track(iT, self._selected_label, annotation, self.txn)
         else:
             logger.info("Mark termination cancelled")
+
+    ################ Daughter selection ################
+    @log_error
+    def daughter_choose_mode_enter_valid(self):
+        iT = self._viewer.dims.current_step[0]
+        track_bboxes_df = self.ta._get_track_bboxes(self._selected_label).reset_index()
+        frames = track_bboxes_df["frame"].values
+        is_valid =  (iT >= frames.min()) and (iT <= frames.max()+1)
+        if not is_valid:
+            logger.info("track does not exist in the current timeframe")
+        return is_valid
+        
+    @log_error
+    def reset_daughter(self):
+        self._daughter_candidates = []
+        self._daughter_frame = self._viewer.dims.current_step[0]
+ 
+    @log_error
+    def on_enter_DAUGHTER_CHOOSE_MODE(self, *_):
+        logger.info("Current daughter candidates count: %i", len(self._daughter_candidates))
+        if len(self.label_child_candidates) == 2:
+            self.finalize_daughter()
+            self.to_LABEL_SELECTED()
+        else:
+            method = choose_division_by_mbox(self.viewer)
+            logger.info("%s selected", method)
+            if method == "select":
+                self.to_DAUGHTER_SWITCH()
+            elif method == "draw":
+                self.refresh_redraw_label_layer()
+                self.to_DAUGHTER_DRAW()
+            else:
+                self.to_LABEL_SELECTED()
+
+    @log_error
+    def daughter_select(self, frame, daughter_label):
+        if frame == self._daughter_frame:
+            self._daughter_candidates.append(daughter_label)
+        else:
+            logger.info("frame not correct")
+
+    @log_error
+    def daughter_draw_finish(self):
+        logger.info("label redraw finish")
+        iT = self._viewer.dims.current_step[0]
+        logger.info("label redraw finish")
+        inds = np.nonzero(self._redraw_layer.data == 1)
+        min_y, min_x, max_y, max_x = inds[0].min(), inds[1].min(), inds[0].max(), inds[1].max()
+        mask = self._redraw_layer.data[min_y:max_y+1, min_x:max_x+1] == 1
+        safe_label = self.ta._get_safe_track_id()
+        self.ta.update_mask(iT, safe_label, (min_y, min_x), mask, self.txn)
+        self._daughter_candidates.append(safe_label)
+
+    @log_error
+    def finalize_daughter(self):
+        assert len(self.label_child_candidates) == 2
+        self.ta.add_split(self._daughter_frame,
+                          self._selected_label, 
+                          self._daughter_candidates, 
+                          self.txn)
