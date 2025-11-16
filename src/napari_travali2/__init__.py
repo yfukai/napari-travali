9322f46a82185852f189e99b389ca2a24b80adac
Yohsuke Fukai <ysk@yfukai.net>
2025-11-16 22:37:48 +0900

    update

--- src/napari_travali2/__init__.py
+++ src/napari_travali2/__init__.py
@@ -1,6 +1,7 @@
 """Napari Travali2."""
 
+from . import actionable_tracks
 from ._stateful_widget import StateMachineWidget
 from ._logging import logger
 
-__all__ = ["StateMachineWidget", "logger"]
+__all__ = ["StateMachineWidget", "logger", "actionable_tracks"]
